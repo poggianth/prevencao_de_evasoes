f7fdffe25b0485d95989fc5add65710be8bc0c74
Thiago <poggianth@gmail.com>
2024-11-21 19:14:18 -0300

    Adicionando rota de estratégias e aprofundar_ideia

--- src/chat_com_cache.js
+++ src/chat_com_cache.js
@@ -41,7 +41,45 @@ const chat = genModel.startChat();
 
 app.get("/", async (req, res) => {
     return res.status(200).json({historico: await chat.getHistory()});
-})
+});
+
+app.get("/estrategias", async (req, res) => {
+    const { motivo } = req.body;
+
+    if(!motivo){
+        return res.status(400).json({ error: "O motivo é obrigatória." });
+    }
+
+    try{
+        const pergunta = `Se baseie nos dados que você recebeu por contexto e me dê 6 possíveis soluções para evitar as evasões onde o motivo é ${motivo}. As 6 soluções devem conter: Título, Descrição e Impacto.  Por exemplo: Título: Descontos por Mérito e Fidalidade; Descrição: Implementar programas de desconto para alunos com excelente desempenho acadêmico ou que permaneçam na instituição por um determinado período.; Impacto: Incentiva a excelência acadêmica e a lealdade dos alunos. Preciso que as soluções sejam retornadas em formato de código JSON, sem markdow e em UTF-8.`;
+
+        const resposta = await chat.sendMessage(pergunta);
+        
+        return res.status(200).json({resposta: resposta.response.text()});
+    }catch(error){
+        console.error("Erro ao processar a pergunta:", error);
+        return res.status(500).json({ error: "Erro ao processar a pergunta." });
+    }
+});
+
+app.get("/aprofundar_estrategia", async(req, res) => {
+    const { estrategia } = req.body;
+
+    if(!estrategia){
+        return res.status(400).json({ error: "A estratégia é obrigatória." });
+    }
+
+    try{
+        const pergunta = `Aprofunde mais a estrategia que você me deu (${estrategia}). Diga as vantagens e desvantagens.`
+
+        const resposta = await chat.sendMessage(pergunta);
+        
+        return res.status(200).json({resposta: resposta.response.text()});
+    } catch(error){
+        console.error("Erro ao processar a pergunta:", error);
+        return res.status(500).json({ error: "Erro ao processar a pergunta." });
+    }
+});
 
 
 app.post("/perguntar", async (req, res) => {
@@ -59,9 +97,9 @@ app.post("/perguntar", async (req, res) => {
         console.error("Erro ao processar a pergunta:", error);
         return res.status(500).json({ error: "Erro ao processar a pergunta." });
     }
-})
+});
 
 
 app.listen(3000, () => {
     console.log("Server run! Port: 3000")
-})
+});
